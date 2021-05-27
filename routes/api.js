4f85f4d38fe3abcd09151e7dae67d144de29949d
MR.D7 <81473463+DAVIDGNZ1@users.noreply.github.com>
2021-05-27 22:11:25 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')
 }
 
-var creatorList = ['@zeeone','@alpha'];
+var creatorList = ['@David','@Dream'];
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 
 
@@ -40,7 +40,7 @@ loghandler = {
         creator: `${creator}`,
         code: 406,
         message: 'masukan parameter apikey',
-        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6283898698875 , key nya gratis kok gan, jadi santuyy ajaa'
+        getApikey: 'gak punya apikey? chat gw aja yaaa , key nya gratis kok gan, jadi santuyy ajaa'
     },
     notkey: {
         status: false,
